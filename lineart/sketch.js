9665f92027543aaa5a0a28994f0f3eb79c8ae51a
alvan alam <alvanalam@gmail.com>
2019-09-09 14:12:09 -0600

    update

---
+++ lineart/sketch.js
@@ -0,0 +1,19 @@
+// line art demo
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let redAmount = 0;
+let redChangeAmount = 1;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+
+}
+
+function draw() {
+  line(mouseX,mouseY,pmouseX,pmouseY)
+}
