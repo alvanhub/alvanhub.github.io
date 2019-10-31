cbf683cdc69a1f0745572a32e1543c7177a37484
alvan alam <alvanalam@gmail.com>
2019-10-31 11:42:32 -0600

    updated

--- 2D grid demo copy/sketch.js
+++ 2D grid demo copy/sketch.js
@@ -8,7 +8,10 @@
 let grid;
 let rows = 30;
 let cols = 30;
-
+let playerX = 15;
+let playerY = 15;
+let yVelocity = 1;
+let xVelocity = 0;
 
 function setup() {
   if (windowWidth > windowHeight) {
@@ -18,6 +21,7 @@ function setup() {
     createCanvas(windowWidth, windowWidth);
   }
   grid = createEmptyGrid(cols, rows);
+  grid[playerY][playerX] = 1;
 }
 
 function draw() {
@@ -35,23 +39,21 @@ function windowResized() {
 }
 
 function keyTyped() {
-  if (key === "r") {
-    grid = createEmptyGrid();
+  playerY += yVelocity;
+  playerX += xVelocity;
+  if (key === "s" && playerY > 0) {
+    yVelocity = -1;
   }
-}
-
-function mousePressed() {
-  let cellSize = width/cols;
-
-  let xCoord = floor(mouseX / cellSize);
-  let yCoord = floor(mouseY / cellSize);
+  if (key === "s" && playerY > 0) {
+    dVelocity = -1;
+  }
+  if (key === "s" && playerY > 0) {
+    dVelocity = -1;
+  }
   
-  if (grid[yCoord][xCoord] === 1) {
-    grid[yCoord][xCoord] = 0;
-  }
-  else {
-    grid[yCoord][xCoord] = 1;
-  }
+
+  // put player back into grid
+  grid[playerY][playerX] = 1;
 }
 
 function createEmptyGrid() {
@@ -65,76 +67,17 @@ function createEmptyGrid() {
   return emptyGrid;
 }
 
-// function update() {
-//   let nextTurn = createEmptyGrid();
-
-//   for (let x = 0; x < cols; x++) {
-//     for (let y = 0; y < rows; y++) {
-//       let neighbors = 0;
-
-//       //loop around the neighbor spots...
-//       for (let i = -1; i <= 1; i++) {
-//         for (let j = -1; j <= 1; j++) {
-//           //deal with edge cases
-//           if (x+i >= 0 && x+i < cols && y+j >= 0 && y+j < rows) {
-//             neighbors += grid[y+j][x+i];
-//           }
-//         }
-//       }
-//       //don't count self as a neighbor
-//       neighbors -= grid[y][x];
-
-//       //apply rules!
-//       if (grid[y][x] === 1) { //currently alive
-//         if (neighbors === 2 || neighbors === 3) {
-//           nextTurn[y][x] = 1;
-//         }
-//         else {
-//           nextTurn[y][x] = 0;
-//         }
-//       }
-
-//       if (grid[y][x] === 0) { //currently dead
-//         if (neighbors === 3) {
-//           nextTurn[y][x] = 1;
-//         }
-//         else {
-//           nextTurn[y][x] = 0;
-//         }
-//       }
-//     }
-//   }
-//   grid = nextTurn;
-// }
-
 function displayGrid(grid, rows, cols) {
   let cellSize = width / cols;
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
-      if (grid[y][x]===0){
+      if (grid[y][x] === 0) {
         fill(255);
       }
       else {
         fill(0);
       }
-      
       rect(x*cellSize, y*cellSize, cellSize, cellSize);
     }
   }
 }
-
-// function create2dArray(cols, rows) {
-//   let randomGrid = [];
-//   for (let x = 0; x < cols; x++) {
-//     randomGrid.push([]);
-//     for (let y = 0; y < rows; y++) {
-//       if (random(100) < 50) {
-//         randomGrid[x].push(1);
-//       }
-//       else {
-//         randomGrid[x].push(0);
-//       }
-//     }
-//   }
-//   return randomGrid;
-// }
