156c91e277eb90d142a6aa2e8ce241c5058f2e10
alvan alam <alvanalam@gmail.com>
2019-11-12 14:15:57 -0600

    updated

--- oop demo 2/sketch.js
+++ oop demo 2/sketch.js
@@ -6,7 +6,7 @@
 // - describe what you did to take this project "above and beyond"
 
 
-let theBullets = [];
+let theFireworks = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -14,37 +14,48 @@ function setup() {
 
 function draw() {
   background(255);
-  for (let i = 0;i< theBullets.length;i++){
-    theBullets[i].move();
-    theBullets[i].display();
+  for (let i = theFireworks.length - 1;i>=0;i--){
+    theFireworks[i].move();
+    if(theFireworks[i].isDone()){
+      theFireworks.splice(i,1)
+    }
+    theFireworks[i].display();
   }
-  if (mouseIsPressed){
-    let myBullet = new Bullet(mouseX,mouseY,random(-3,3),random(-3,3), 10 * 2);
-    theBullets.push(myBullet);
-  }
+  // if (mouseIsPressed){
+  //   let myFirework = new Bullet(mouseX,mouseY,random(-3,3),random(-3,3), 10 * 2);
+  //   theFireworks.push(myFirework);
+  // }
 }
 
-// function mousePressed() {
-//   let myBullet = new Bullet(mouseX,mouseY,random(-3,3),random(-3,3), 10 * 2);
-//   theBullets.push(myBullet);
-// }
+ function mousePressed() {
+   for(let i = 0; i < 100; i++) {
+     let myFirework = new Bullet(mouseX,mouseY,random(-3,3),random(-3,3), 10 * 3);
+     theFireworks.push(myFirework);
+    }
+ }
 
-class Bullet {
+class Particle {
   constructor(x,y,dx,dy,radius) {
     this.x = x;
     this.y = y;
     this.dx = dx;
     this.dy = dy;
     this.radius = radius;
+    this.alpha = 255;
   }
 
   display() {
-    fill(0);
+    fill(255,0,0,this.alpha);
     circle(this.x,this.y,this.radius * 2);
   }
 
   move() {
     this.x += this.dx;
     this.y += this.dy;
+    this.alpha -= 1;
+  }
+
+  isDone() {
+    return this.alpha <= 0;
   }
 }
