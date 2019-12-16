ded4048a5e1035cf9af7e720639845d6c5b3064c
alvan alam <alvanalam@gmail.com>
2019-12-16 14:12:04 -0600

    updated

--- Interactive Scene/sketch.js
+++ Interactive Scene/sketch.js
@@ -1,5 +1,5 @@
 // Interactive Assignment
-// Alva Alam
+// Alvan Alam
 // 9/25/2019
 //
 // Extra for Experts:
@@ -7,16 +7,16 @@
 // Technically i also used the beginShape() and endShape() functions to make the shapes trail
 
 //variables used to set the shapes coordinates,rotation,stroke weight, and movement
-let coordx;
-let coordy;
+let coordX;
+let coordY;
 
 let a;
 
 let pos = 1;
   
-  let movey;
-  let movex;
-  let speed;
+let moveY;
+let moveX;
+let speed;
 
 //an array used to store the shapes path
 let trail = [];
@@ -26,8 +26,8 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 
   //variables that set the shapes starting position and starting speed
-  coordx = width/2;
-  coordy = height/2;
+  coordX = width/2;
+  coordY = height/2;
   speed = 0;
 
 }
@@ -40,64 +40,58 @@ function draw() {
 
   create();
 
-  Forward();
+  forward();
   
   pop();
 
-  Trail();
+  shapeTrail();
   
 }
 
 //mouseWheel function that implements the mouse wheel to change the shapes stroke weight
 function mouseWheel(event) {
-  
-  
   if (event.delta > 0) {
     pos += 1;
   }
   else {
     pos -= 1;
   }
-
-  
-  
   return false;
 }
 
 //function that does the math for the shapes movement and controls the shapes movement, increasing the shapes x and y coordinates by the amount neccessary to move it towards the mouse
-function Forward() {
+function forward() {
   if (keyIsDown(UP_ARROW)){
-
-    movex = speed*(cos(a));
-    movey = speed*(sin(a));
+    
+    moveX = speed*(cos(a));
+    moveY = speed*(sin(a));
      
-    coordx += movex;
-    coordy += movey;
+    coordX += moveX;
+    coordY += moveY;
     
     //this part of the code creates acceleration for the shape
     if (speed < 15){
       speed += 0.2;
     }
   }
-     
-   
-    else {
-      movex = speed*(cos(a));
-      movey = speed*(sin(a));
   
-      coordx += movex;
-      coordy += movey;
-      
-      //this part of code creates deceleration for the shape
-      if (speed <= 0) {
-        speed += 0.2;
-      }
-      speed -= 0.2;
+  else {
+    moveX = speed*(cos(a));
+    moveY = speed*(sin(a));
+    
+    coordX += moveX;
+    coordY += moveY;
+    
+    //this part of code creates deceleration for the shape
+    if (speed <= 0) {
+      speed += 0.2;
+    }
+    speed -= 0.2;
   }
 }
 
 //function that creates the shapes trail and controls the trails length
-function Trail() {
+function shapeTrail() {
   beginShape();
   strokeWeight(pos);
   stroke(0);
@@ -117,16 +111,17 @@ function Trail() {
 
 //function that creates the shapes coordinates and stores them aswell as creating the shapes rotation towards the mouse and the shape itself
 function create() {
-  translate(coordx, coordy);
+  translate(coordX, coordY);
   background(220);
   
-   let position = {
-      x : coordx,
-      y : coordy
-    };
+  let position = {
+    x : coordX,
+    y : coordY
+  };
+
   trail.push(position);
   
-  a = atan2(mouseY - coordy, mouseX - coordx);
+  a = atan2(mouseY - coordY, mouseX - coordX);
   rotate(a);
   
   
